0f8229d96e4d71d87d66a60264f1d2c7a4422ad0
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2020-12-03 01:05:10 +0100

    Modify Program.cs to read provided input arguments and invoke `Solver.SolveLast()` (new default), `Solver.SolveAll()` or `Solver.Solve(args)` where appropiated.

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,3 +1,17 @@
 ﻿using AoCHelper;
+using System.Linq;
 
-Solver.SolveAll();
+if (args.Length == 0)       // dotnet run
+{
+    Solver.SolveLast();
+}
+else if (args.Length == 1 && args[0].Contains("all", System.StringComparison.CurrentCultureIgnoreCase))     // dotnet run all | dotnet run --all
+{
+    Solver.SolveAll();
+}
+else        // dotnet run 1 2 5 10
+{
+    var indexes = args.Select(arg => uint.TryParse(arg, out var index) ? index : uint.MaxValue);
+
+    Solver.Solve(indexes.Where(i => i < uint.MaxValue));
+}
